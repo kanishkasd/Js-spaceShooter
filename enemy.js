594e2fd73be085f3c0b94882a98d5a87d7a2967f
kanishkasd <kanishkasandaru2048@gmail.com>
2024-01-20 08:36:33 +0530

    simple changes

--- enemy.js
+++ enemy.js
@@ -1,15 +1,25 @@
+
 class Enemy {
-    constructor(game){
+    constructor(game) {
         this.game = game;
-        this.x;
-        this.y;
-        this.speedX;
-        this.speedY;
+        this.x = Math.random() * this.game.width;
+        this.y =  Math.random() * this.game.height;
+        this.speedX = 0;
+        this.speedY = Math.random() * 4 + 1;
         this.width = 50;
         this.height = 50;
-    }
-    draw(){
 
     }
-    
+    draw() {
+        this.game.ctx.fillStyle = "red";
+        this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+        
+        if(this.y > this.game.height){
+            this.y = -this.height;
+        }
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
--- main.js
+++ main.js
@@ -1,42 +1,65 @@
 
 class Game {
-    constructor(canvas) {
+    constructor(canvas, ctx) {
         this.canvas = canvas;
+        this.ctx = ctx;
         this.width = this.canvas.width;
-        this.height = this.canvas.height;  // ensure that width & height of the canvas same as canvas
-        this.posX = 50;
+        this.height = this.canvas.height;
+        this.posX = 0;
 
-        window.addEventListener('resize', e => { // e gives us the event of the current window
+        this.enemy1 = new Enemy(this);
+        this.enemy2 = new Enemy(this);
+        this.enemy3 = new Enemy(this);
+        this.enemy4 = new Enemy(this);
+        this.enemy5 = new Enemy(this);
+
+        this.start()
+
+        window.addEventListener('resize', e => { // we use e keyword to get this object not the window
             this.resize(e.target.innerWidth, e.target.innerHeight)
         });
+    }
+
+    start() {
+        this.resize(window.innerWidth, window.innerHeight);
     }
     resize(width, height) {
         this.canvas.width = width;
         this.canvas.height = height;
         this.width = width;
         this.height = height;
+        this.ctx.fillStyle = "green";
     }
 
-    render(context) {
-       this.posX++; 
-        context.fillRect(this.posX, 100, 50, 150);
+    render() {
+        this.enemy1.draw();
+        this.enemy1.update();
+        this.enemy2.draw();
+        this.enemy2.update();
+        this.enemy3.draw();
+        this.enemy3.update();
+        this.enemy4.draw();
+        this.enemy4.update();
+        this.enemy5.draw();
+        this.enemy5.update();
     }
 }
 
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1')
-    const ctx = canvas.getContext('2d')
+    const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.fillStyle = 'green'
 
-    const game = new Game(canvas);
+    const game = new Game(canvas, ctx);
 
-    function animate(){
-        ctx.clearRect(0, 0 , canvas.width, canvas.height)
-        game.render(ctx)
-        requestAnimationFrame(animate)
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.render(ctx);
+        requestAnimationFrame(animate) // call animate function recusively
     }
     requestAnimationFrame(animate)
-
 })
+
+
+
